1f414596b2ac7fd110c12251831ed0ee7419e1ac
Eric Sund <epsund@gmail.com>
2021-05-12 22:12:50 -0700

    new UI with good thing

--- HelloWorld/HueAPI.cs
+++ HelloWorld/HueAPI.cs
@@ -38,9 +38,9 @@ namespace HueAPI
     {
         private static readonly string baseUrl = "http://192.168.0.16/api/z01OPw01ouRwbecqso7V1sFcuea4PC37pSs4lrXW/lights/";
 
-        public static void ToggleOnOff(int LightNum)
+        public static void ToggleOnOff(string LightNum)
         {
-            string url = $"{baseUrl}{LightNum.ToString()}/state";
+            string url = $"{baseUrl}{LightNum}/state";
 
             // get the state of the light
             string res = APICall.Get(baseUrl);
@@ -58,54 +58,81 @@ namespace HueAPI
             }
         }
 
-        public static void ForceAllOnOff(HelloWorld.LightsGroupPage.Group g, bool on)
+        public static void ForceAllOnOff(string[] LightIdList, bool on)
         {
-            string res = APICall.Get(baseUrl);
-            dynamic resDeserialized = JsonConvert.DeserializeObject(res);
-
-            foreach (string currLightId in g.LightIds)
+            foreach (string currLightId in LightIdList)
             {
-                // get the state of the light    
-                string resParsed = resDeserialized[currLightId.ToString()]["state"]["on"].ToString();
                 // create url to change light status
-                string url = $"{baseUrl}{currLightId.ToString()}/state";
+                string url = $"{baseUrl}{currLightId}/state";
 
                 if (on)
                 {
                     APICall.Put(url, "{\"on\":true}");
-                    g.AllOn = true;
                 }
                 else
                 {
                     APICall.Put(url, "{\"on\":false}");
-                    g.AllOn = false;
                 }
             }
         }
 
-        public static dynamic FetchLightInfo(int LightNum)
+        public static dynamic FetchLightInfo(string LightNum)
         {
-            string url = $"{baseUrl}{LightNum.ToString()}";
+            string url = $"{baseUrl}{LightNum}";
             string res = APICall.Get(url);
             dynamic resDeserialized = JsonConvert.DeserializeObject(res);
             return resDeserialized;
         }
 
-        public static void SetLightBrightness(int LightNum, int NewBri)
+        public static void SetLightBrightness(string LightNum, string NewBri)
         {
-            string url = $"{baseUrl}{LightNum.ToString()}/state";
-            APICall.Put(url, "{\"bri\":" + NewBri.ToString() + "}");
+            string url = $"{baseUrl}{LightNum}/state";
+            APICall.Put(url, "{\"bri\":" + NewBri + "}");
+        }
+
+        public static void ToggleRainbowMode(string LightNum)
+        {
+            string url = $"{baseUrl}{LightNum}/state";
+
+            // get the state of the light
+            string res = APICall.Get(baseUrl);
+            dynamic resDeserialized = JsonConvert.DeserializeObject(res);
+            string currentEffect = resDeserialized[LightNum]["state"]["effect"].ToString();
+
+            if (currentEffect.Equals("colorloop"))
+            {
+                HueAPI.APICall.Put(url, "{\"effect\":\"none\"}");
+            }
+            else
+            {
+                HueAPI.APICall.Put(url, "{\"effect\":\"colorloop\"}");
+            }
         }
     }
 
     class Groups
     {
         private static readonly string baseUrl = "http://192.168.0.16/api/z01OPw01ouRwbecqso7V1sFcuea4PC37pSs4lrXW/groups/";
+        
         public static dynamic GetGroups()
         {
             string res = APICall.Get(baseUrl);
             dynamic resDeserialized = JsonConvert.DeserializeObject(res);
             return resDeserialized;
+        }
+
+        public static dynamic GetGroup(string GroupNum)
+        {
+            string res = APICall.Get(baseUrl + GroupNum);
+            dynamic resDeserialized = JsonConvert.DeserializeObject(res);
+            return resDeserialized;
+        }
+
+        public static Boolean AllOn(string GroupNum)
+        {
+            string res = APICall.Get(baseUrl + GroupNum);
+            dynamic resDeserialized = JsonConvert.DeserializeObject(res);
+            return resDeserialized["state"]["all_on"];
         }
     }
 }
--- HelloWorld/LightsGroupPage.xaml.cs
+++ HelloWorld/LightsGroupPage.xaml.cs
@@ -30,103 +30,238 @@ namespace HelloWorld
 
     public sealed partial class LightsGroupPage : Page
     {
-        List<GroupHeader> headers = new List<GroupHeader>();
+        List<LightHeader> headers = new List<LightHeader>();
+        string ACTIVE_GROUP = "1";
+        Boolean FLOODLIGHTS_ON;
 
-
-        public class GroupHeader
+        public class LightHeader
         {
-            public GroupHeader()
+            public LightHeader()
             {
-                Groups = new ObservableCollection<Group>();
+                Lights = new ObservableCollection<LightObj>();
             }
 
             public string Name { get; set; }
-            public ObservableCollection<Group> Groups { get; private set; }
+            public string OnOffState { get; set; }
+            public string LightId { get; set; }
+            public string ColorloopActive { get; set; }
+            public Boolean firstRainbowToggle { get; set; }
+            public Boolean firstOnOffToggle { get; set; }
+            public int Brightness { get; set; }
+
+            public ObservableCollection<LightObj> Lights { get; private set; }
         }
 
-        public class Group
+        public class LightObj
         {
-            public string NumLights { get; set; }
-            public string[] LightIds { get; set; }
-            public bool AllOn { get; set; }
+            public string LightId { get; set; }
+            public string Name { get; set; }
+            public string OnOffState { get; set; }
+            public int Brightness { get; set; }
+        }
+
+        public static void updateOnOffStateUI(LightObj light)
+        {
+            string state = HueAPI.Lights.FetchLightInfo(light.LightId)["state"]["on"];
+
+            if (state.Equals("True"))
+            {
+                light.OnOffState = "Switched on";
+            }
+            else
+            {
+                light.OnOffState = "Switched off";
+            }
+        }
+
+        public static void toggleOnOffState(LightObj light)
+        {
+            HueAPI.Lights.ToggleOnOff(light.LightId);
+            updateOnOffStateUI(light);
         }
 
         public LightsGroupPage()
         {
             this.InitializeComponent();
-            PopulateLightsData();
-            PopulateLightsData_Helper();
+            PopulateAllGroupNames();
+            PopulateFloodlightsToggle();
         }
 
-        private void PopulateLightsData()
+        private void PopulateAllGroupNames()
         {
-            dynamic groupsInfo = Groups.GetGroups();
+            dynamic groupsInfo = HueAPI.Groups.GetGroups();
             
             int i = 1;
             while (groupsInfo[i.ToString()] != null)
             {
-                GroupHeader group1 = new GroupHeader();
-                group1.Name = groupsInfo[i.ToString()]["name"];
+                string currName = groupsInfo[i.ToString()]["name"];
+                MenuFlyoutItem currItem = new MenuFlyoutItem();
+                currItem.Name = i.ToString(); // store the Id in the Name atrribute
+                currItem.Text = currName; // store the actual group name in the text attribute
+                currItem.Click += SwitchGroupDisplay;
 
-                // find all lights in the current group
-                Group g1 = new Group();
-                g1.AllOn = true;
-                g1.LightIds = groupsInfo[i.ToString()]["lights"].ToObject<string[]>();
-                g1.NumLights = groupsInfo[i.ToString()]["lights"].Count.ToString();
-                group1.Groups.Add(g1);
-
-                headers.Add(group1);
-
+                GroupsList.Items.Add(currItem);
                 i++;
             }
+
+            // by default, display the first group from the query response
+            PopulateSingleGroupControls(ACTIVE_GROUP);
+        }
+
+        private void PopulateSingleGroupControls(string GroupId)
+        {
+            dynamic groupsInfo = HueAPI.Groups.GetGroup(GroupId);
+
+            // grab all the lights info in the specified group
+            string[] lightIdList = groupsInfo["lights"].ToObject<string[]>();
+
+            foreach (string lightId in lightIdList)
+            {
+                LightHeader currLightHeader = new LightHeader();
+                string currName = HueAPI.Lights.FetchLightInfo(lightId)["name"];
+                currLightHeader.Name = currName;
+                currLightHeader.LightId = lightId;
+                currLightHeader.Brightness = HueAPI.Lights.FetchLightInfo(lightId)["state"]["bri"];
+
+                currLightHeader.firstOnOffToggle = true;
+                currLightHeader.firstRainbowToggle = true;
+                PopulateRainbowSingleLightToggle(currLightHeader);
+                PopulateSingleLightToggle(currLightHeader);
+
+                LightObj currLight = new LightObj();
+                currLight.LightId = lightId;
+                currLight.Brightness = HueAPI.Lights.FetchLightInfo(lightId)["state"]["bri"];
+                updateOnOffStateUI(currLight);
+
+                currLightHeader.Lights.Add(currLight);
+                headers.Add(currLightHeader);
+            }
+
+            ACTIVE_GROUP = GroupId;
+            Debug.WriteLine("populating group controls for " + GroupId);
 
             cvsProjects.Source = headers;
         }
 
-        private void PopulateLightsData_Helper()
+        private void PopulateFloodlightsToggle()
         {
-            List<string> lightsInGroup = new List<string>();
-            //lightsInGroup.AddRange(groupsInfo[i.ToString()]["lights"].ToObject<string[]>());
-            lightsInGroup.Add("1");
-            lightsInGroup.Add("2");
-            lightsInGroup.Add("3");
-        }
-
-        private void GroupBrightnessChanged(object sender, ManipulationCompletedRoutedEventArgs e)
-        {
-            Slider MySlider = sender as Slider;
-            double newBrightness = MySlider.Value;
-
-            Group h = headers[0].Groups[0];
-            foreach (string currLightId in h.LightIds)
+            if (HueAPI.Groups.AllOn(ACTIVE_GROUP))
             {
-                HueAPI.Lights.SetLightBrightness(int.Parse(currLightId), (int)newBrightness);
+                FLOODLIGHTS_ON = true;
             }
         }
 
+        private void PopulateSingleLightToggle(LightHeader light)
+        {
+            dynamic currLightInfo = HueAPI.Lights.FetchLightInfo(light.LightId);
+            light.OnOffState = currLightInfo["state"]["on"].ToString();
+        }
+
+        private void PopulateRainbowSingleLightToggle(LightHeader light)
+        {
+            dynamic currLightInfo = HueAPI.Lights.FetchLightInfo(light.LightId);
+            string currEffect = currLightInfo["state"]["effect"];
+            light.ColorloopActive = currEffect.Equals("colorloop") ? "True" : "False";
+        }
+
+        private void SwitchGroupDisplay(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem menuItem = sender as MenuFlyoutItem;
+            PopulateSingleGroupControls(menuItem.Name);
+        }
+
+        /*        private void GroupBrightnessChanged(object sender, ManipulationCompletedRoutedEventArgs e)
+                {
+                    Slider MySlider = sender as Slider;
+                    double newBrightness = MySlider.Value;
+
+                    Group h = headers[0].Groups[0];
+                    foreach (string currLightId in h.LightIds)
+                    {
+                        HueAPI.Lights.SetLightBrightness(int.Parse(currLightId), (int)newBrightness);
+                    }
+                }*/
+
         private void SingleSwitch(object sender, ItemClickEventArgs e)
         {
-
+            LightObj res = e.ClickedItem as LightObj;
+            // todo the UI does not update
+            toggleOnOffState(res);
         }
 
         private void SingleBrightnessChanged(object sender, ManipulationCompletedRoutedEventArgs e)
         {
             Slider MySlider = sender as Slider;
-            string name = MySlider.Name;
-            Debug.WriteLine(name);
+            string LightId = MySlider.AccessKey;
+
+            double newBrightness = MySlider.Value;
+
+            HueAPI.Lights.SetLightBrightness(LightId, newBrightness.ToString());
         }
 
-        private void GroupSwitch(object sender, ItemClickEventArgs e)
+        private void SingleLight_Toggle(object sender, RoutedEventArgs e)
         {
-            Group g = headers[0].Groups[0];
-            if (g.AllOn)
+            ToggleSwitch MyToggle = sender as ToggleSwitch;
+            string currLightId = MyToggle.AccessKey;
+
+            foreach (LightHeader header in headers)
             {
-                HueAPI.Lights.ForceAllOnOff(g, false);
+                if (header.LightId.Equals(currLightId))
+                {
+                    bool lightOn = HueAPI.Lights.FetchLightInfo(header.LightId)["state"]["on"].ToString().Equals("True");
+                    if (header.firstOnOffToggle && !lightOn)
+                    {
+                        header.firstOnOffToggle = false;
+                        HueAPI.Lights.ToggleOnOff(MyToggle.AccessKey);
+                    }
+                    else if (header.firstOnOffToggle)
+                    {
+                        header.firstOnOffToggle = false;
+                    }
+                    else
+                    {
+                        HueAPI.Lights.ToggleOnOff(MyToggle.AccessKey);
+                    }
+                }
             }
-            else
+        }
+
+        private void Rainbow_Toggle(object sender, RoutedEventArgs e)
+        {
+            ToggleSwitch MyToggle = sender as ToggleSwitch;
+            string currLightId = MyToggle.AccessKey;
+
+            foreach (LightHeader header in headers)
             {
-                HueAPI.Lights.ForceAllOnOff(g, true);
+                if (header.LightId.Equals(currLightId))
+                {
+                    bool noEffect = HueAPI.Lights.FetchLightInfo(header.LightId)["state"]["effect"].ToString().Equals("none");
+                    if (header.firstRainbowToggle && noEffect)
+                    {
+                        header.firstRainbowToggle = false;
+                        HueAPI.Lights.ToggleRainbowMode(MyToggle.AccessKey);
+                    }
+                    else if (header.firstRainbowToggle)
+                    {
+                        header.firstRainbowToggle = false;
+                    }
+                    else
+                    {
+                        HueAPI.Lights.ToggleRainbowMode(MyToggle.AccessKey);
+                    }
+                }
             }
+        }
+
+        private void Floodlights_Toggle(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem MyToggle = sender as MenuFlyoutItem;
+
+            dynamic groupInfo = HueAPI.Groups.GetGroup(ACTIVE_GROUP);
+            string[] lightIdList = groupInfo["lights"].ToObject<string[]>();
+
+            FLOODLIGHTS_ON = FLOODLIGHTS_ON == true ? false : true;
+            HueAPI.Lights.ForceAllOnOff(lightIdList, FLOODLIGHTS_ON);
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
